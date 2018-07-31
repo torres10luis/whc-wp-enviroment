42f6c14d2a887a9a48c1e488dcd7e2b9e209b0ce
torres10luis <Luis.angel_15@yahoo.com>
2018-07-31 14:18:48 -0700

    enviroment commit added pages

---
+++ wordpress/wp-content/themes/whc-e/home-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all single posts 
+ * 
+ * Template Name: Home Width Page Temp
+ * 
+ */
+
+get_header();
+?>
+    <div class="col-md-8">
+           <h1>Hello welcome to the Home Page!!</h1>    
+           <p>Please Feel free to ask question</p>
+    </div>
+
+<?php
+get_sidebar();
+get_footer();
